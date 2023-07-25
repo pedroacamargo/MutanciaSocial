cd9e168edd017241e1ab3d0cae991b82ad219454
pedroacamargo <pedroaugennes@gmail.com>
2023-07-25 14:41:59 +0100

    React Helmet to fix warning

--- src/app/GlobalStyles.styles.tsx
+++ src/app/GlobalStyles.styles.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&family=Oswald:wght@400;500&display=swap');
-
     :root {
         --font1: 'Montserrat', sans-serif;
         --font2: 'Oswald', sans-serif;
