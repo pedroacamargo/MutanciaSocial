428209bfeddc0d52627b0106827f2815f23f35ae
pedroacamargo <pedroaugennes@gmail.com>
2023-07-28 18:37:38 +0100

    Auth functions documentation

--- src/app/_components/auth/Auth.server.ts
+++ src/app/_components/auth/Auth.server.ts
@@ -1,6 +1,10 @@
 import { auth } from "@/utils/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
+/**
+ * 
+ * @returns boolean: Returns true if theres an user logged in and false if not 
+ */
 export async function isLoggedIn() {
     return new Promise((resolve) => {        
         const unsubscribe = onAuthStateChanged(auth , (user) => {
@@ -11,7 +15,11 @@ export async function isLoggedIn() {
     })
 }
 
-
+/**
+ * Will check if an user is logged in, if is logged and there's an user stored in localStorage as "currentUser", will parse the JSON and return the user data.
+ * Use this function with a dispatch(setCurrentUser(await statePersist())) in a useEffect to persist the data in Redux
+ * @returns JSON.parse(window.localStorage.getItem("currentUser"))
+ */
 export async function statePersist() {
     const statePersistFunc = async () => {
         const isLogged = await isLoggedIn();
